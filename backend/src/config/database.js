92211f1b094424099c1ddd7723ef1ce88d3bcc69
Felipe Cabral <felipeo.cabral@hotmail.com>
2020-10-23 21:40:34 -0300

    feat: added web frontend

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -4,6 +4,7 @@ module.exports = {
   dialect: 'postgres',
   host: 'localhost',
   username: 'postgres',
+  port: 5400,
   password: process.env.DATABASE_PASSWORD,
   database: process.env.DATABASE_NAME,
   define: {
