b2a4cf56daf14989166e63f671ffc52499bb35f5
Felipe Cabral <felipeo.cabral@hotmail.com>
2020-11-11 05:29:12 -0300

    feat: redis container and proxy setup

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -2,11 +2,11 @@ require('dotenv').config()
 
 module.exports = {
   dialect: 'postgres',
-  host: 'localhost',
+  host: process.env.POSTGRES_HOST,
   username: 'postgres',
   port: 5432,
-  password: process.env.DATABASE_PASSWORD,
-  database: process.env.DATABASE_NAME,
+  password: process.env.POSTGRES_PASSWORD,
+  database: process.env.POSTGRES_DB,
   define: {
     timestamps: true,
     underscored: true,
--- backend/src/config/redis.js
+++ backend/src/config/redis.js
@@ -1,6 +1,8 @@
+require('dotenv').config()
+
 const redisConfig = {
-  host: 'localhost',
-  port: 6379,
+  host: process.env.REDIS_HOST,
+  port: process.env.REDIS_PORT,
   password: undefined,
 }
 
